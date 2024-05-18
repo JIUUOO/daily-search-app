d03e5814ea793ca6ea6b33265c50a60690e50ecd
JIUUOO <nninjiuuoo@gmail.com>
2024-05-18 21:13:34 +0900

    test: deploy

--- src/App.js
+++ src/App.js
@@ -2,22 +2,26 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const Word = "devoid / ~가 없는";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <form>
+          Searh on Google!
+          <input placeholder={Word}></input>
+          <button>Search</button>
+        </form>
+        <form>
+          Searh on Google!
+          <input placeholder={Word}></input>
+          <button>Search</button>
+        </form>
+        <form>
+          Searh on Google!
+          <input placeholder={Word}></input>
+          <button>Search</button>
+        </form>
+      </div>
     </div>
   );
 }
